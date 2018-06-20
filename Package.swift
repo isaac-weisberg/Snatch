eb688e63a74009d00991fa83ed6b63640419941f
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-06-20 20:51:17 +0700

    Add a package declaration and a makefile so it's comfy to work on Linux

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Snatch",
+    products: [
+        .library(
+            name: "Snatch",
+            targets: ["Snatch"]),
+    ],
+    dependencies: [
+        
+    ],
+    targets: [
+        .target(
+            name: "Snatch",
+            dependencies: [  ])
+    ]
+)
